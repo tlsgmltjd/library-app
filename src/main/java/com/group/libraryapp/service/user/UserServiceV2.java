b452c9fd86d06e106b7c1c9fab33d8447cedbcb2
신희성 <vvwv3092@gmail.com>
2023-08-30 23:15:29 +0900

    Add Transaction

--- src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -6,6 +6,7 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,11 +19,18 @@ public class UserServiceV2 {
         this.userRepository = userRepository;
     }
 
+    // 트랜잭션 등록
+    // 아래 있는 함수가 실행될 때 start transaction을 해준다.
+    // 함수가 예외 없이 잘 끝났다면 commit
+    // 혹시라도 문제가 있다면 rollback
+    @Transactional
     public void saveUser(UserCreateRequest request) {
         // save 메소드에 객체를 넣어주면 INSERT SQL이 자동으로 날라간다.
         userRepository.save(new User(request.getName(), request.getAge()));
     }
 
+    // SELECT 쿼리만 사용한다면, readOnly 옵션을 쓸 수 있다.
+    @Transactional(readOnly = true)
     public List<UserResponse> getUsers() {
         // findAll 을 사용하면 모든 데이터를 가져온다.
         List<User> users = userRepository.findAll();
@@ -31,21 +39,21 @@ public class UserServiceV2 {
                 .collect(Collectors.toList());
     }
 
+    @Transactional
     public void updateUser(UserUpdateRequest request) {
         // select * from user where id = ?;
         // Optional<User>
         User user = userRepository.findById(request.getId()).orElseThrow(IllegalArgumentException::new);
 
         user.updateName(request.getName());
-        userRepository.save(user);
+        // userRepository.save(user); <- 영속성 컨텍스트가 엔티티의 변경을 감지해 자동으로 저장해줌
     }
 
+    @Transactional
     public void deleteUser(String name) {
         // select * from user where name = ?;
-        User user = userRepository.findByName(name);
-        if (user == null) {
-            throw new IllegalArgumentException();
-        }
+
+        User user = userRepository.findByName(name).orElseThrow(IllegalArgumentException::new);
 
         userRepository.delete(user);
     }
